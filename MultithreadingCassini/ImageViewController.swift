e7aa1be23091bb518519ef65d95229ea9e889ca1
MB <mb@mbs-imac.local>
2019-04-28 17:00:58 +0530

    DispatchQueue.global(qos : .userInitiated).async {  }    // To execute the code off the main queue DispatchQueue.main.async{}               //to dispatch the code off to the main queue  // because we do Ui stuffs on main thread

--- MultithreadingCassini/ImageViewController.swift
+++ MultithreadingCassini/ImageViewController.swift
@@ -78,60 +78,46 @@ class ImageViewController: UIViewController , UIScrollViewDelegate {
     func fetchImage(){
         
         if let url = imageURL{
-            /*
-             This is where our app is trying to go out on the get that cassini or any URL and it's big
-             And even for a faster network it's taking long to load over
-             So this line of code is aint returning
-             let urlContents = try? Data(contentsOf: url)
-             So I cant have this line of code executing on main queue
-             which is where all my code pretty much runs unless I specifically put it somewhere else or  unless I use some iOS API to  put it somewhere else
-             
-             So how do I get it off the main queue ?
-             Well I just use
-             DispatchQueue.main.async{}
-             
-             but we don't want to run on main queue So lets change it
-             DispatchQueue.global(qos : .userInitiated).async{}
-             
-             this will immediately stop my app from blocking like that because it's going to be doing this network fetch on some other queue
-             */
             
             DispatchQueue.global(qos: .userInitiated).async { [weak self] in
                 let urlContents = try? Data(contentsOf: url)
                 
-                if let imageData = urlContents{
-                    
-                    self?.image = UIImage(data: imageData)
-                    /*
-                     Checking if we have a memory cycle here
-                     here self doesnt have a pointer to this closure
-                     there is no pointer in self that points to this closure
-                     So there's no cycle
-                     
-                     However whenever you do this with multithreading
-                     when you do self. you have to think about another thing
-                     which is what happens if the code before this line that has self itself takes a long time to execute
-                     So long that this viewcontroller doesn't even wanna be in here anymore
-                     Can't you imagine that easily happening
-                     Someone goes to go get an image
-                     The image is taking 5 minutes to come along they are like forget it
-                     and they will click somewhere else , go back or something
-                     Now that view controller that requested that image it's meaningless
-                     It has no value it should not be in the heap but it's being kept in the heap by  this closure
-                     because I have a reference to self in this closure , this view controller ,
-                     my imageViewcontroller is being always kept in the heap
-                     for as long as image request is outstanding  ,
-                     So this is a case where i wanna do weak self
-                     Not having anything to do with memory cycles
-                     but having to do with fact that i don't want self held in the heap by this closure
-                     if this closure takes so long to run that the user doesn't care about self anymore then Idc about self anymore
-                     So by definition i want to be weak
-                     
-                     
-                     So in multithread think about weak or whether you wanna really hold things on heap
-                     
-                    */
+                DispatchQueue.main.async {
+                    if let imageData = urlContents{
+                        
+                        self?.image = UIImage(data: imageData)
+                        
+                    }
                 }
+
+                /*
+                 while this will stop my UI from blocking it will probably screwup my UI
+                 probably cause my UI to draw all funny or get completely wedged and why is that?
+                 
+                 because this line of code
+                 self?.image = UIImage(data: imageData)
+                 because here I am setting image
+                 and in line 30 set of private var image
+                 we are setting
+                 imageView.image which is a UI thing
+                 imageView.sizeToFit() sizzing to fit which is a UI thing
+                 scrollView?.contentSize = imageView.frame.size which is setting scrollView contentArea which is a  Ui thing
+                 
+                 So we are doing all kind of UI stuffs when I set this image
+                 So I cant do this on the queue that I put this code on
+                 
+                 This
+                 DispatchQueue.global(qos: .userInitiated).async
+                 is not a UI queue
+                 i cant do it there,
+                 So what I have to do here is
+                 DispatchQueue.main.async{}
+                 
+                 This dispatches the code back to main queue
+                 Now it's gonna get in line and run on the main queue when the main queue is quiet
+                 */
+                
+                
             }
         }
     }

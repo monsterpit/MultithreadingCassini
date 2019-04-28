bbd3754a4a23920467b34fdc2a0f2ea4d0e47f0e
MB <mb@mbs-imac.local>
2019-04-28 18:46:29 +0530

    Cleaning up segue destination code using extension

--- MultithreadingCassini/CassiniViewController.swift
+++ MultithreadingCassini/CassiniViewController.swift
@@ -18,13 +18,7 @@ class CassiniViewController: UIViewController {
             //getting url from constant DemoUrl if segue identifier name key was present
             if let url = DemoURLs.NASA[identifier]{
                 
-                var destination = segue.destination
-                
-                if let navcon = destination as? UINavigationController{
-                    destination = navcon.visibleViewController ?? navcon
-                }
-                
-                if let imageVC = destination as? ImageViewController{
+                if let imageVC = segue.destination.contents as? ImageViewController{
 
                     imageVC.imageURL = url
                     
@@ -40,12 +34,18 @@ class CassiniViewController: UIViewController {
 
 
 
-/*
- So after running Ui was getting stuck when trying to rotate my Ui was just blank doing nothing
- So our UI is completely stuck
- No matter where I touch even if I rotate nothinhg happened
- Well this ia horrendous experience for your user
- This is the kind of experience that will cause your user to go and delete app
- Because you cannot have your UI freezing up so that's why we have multi threaded as such an important piece of the kind of app development do in iOS
- So lets fix this with multi threading 
- */
+extension UIViewController{
+    var contents : UIViewController{
+        if let navcon = self as? UINavigationController{
+            return navcon.visibleViewController ?? self
+        }
+            // For TabBar
+//        else if let navcon = self as? UITabBarController{
+//            return navcon.selectedViewController ?? self
+//        }
+        else{
+            return self
+        }
+    }
+}
+
